5cdd934e7e73f20f9618955b5f855d228ad9b183
thefirethirteen <florea13andrew@gmail.com>
2021-03-20 19:36:26 +0200

    update rps.py

--- resources/rps.py
+++ resources/rps.py
@@ -5,47 +5,56 @@ def rockPaperScissors(USER_NAME1, USER_NAME2):
     
     while txt:
         import getpass
-        U1 = 0
-        U2 = 0
+        roundCounter = 0
+        user_one_wins = 0
+        user_two_wins = 0
     
-        while U1 <= 10 and U2 <= 10:
-            USER1 = getpass.getpass("Rock, paper or scissors ? ", None)
-            USER2 = getpass.getpass("Rock, paper or scissors ? ", None)
+        while roundCounter <= 10:
+            USER1 = getpass.getpass("Rock, paper or scissors? ", None)
+            USER2 = getpass.getpass("Rock, paper or scissors? ", None)
     
             if USER1 in ["rock", "r"]:
                 if USER2 in ["paper", "p"]:
                     print(USER_NAME2, " won!", "\nCongratulations!")
-                    U2 += 1
+                    roundCounter += 1
+                    user_two_wins += 1
                 if USER2 in ["scissors", "s"]:
                     print(USER_NAME1, " won!", "\nCongratulations!")
-                    U1 += 1
-            if USER1 in ["paper", "p"]:
+                    roundCounter += 1
+                    user_one_wins += 1
+            elif USER1 in ["paper", "p"]:
                 if USER2 in ["rock", "r"]:
                     print(USER_NAME1, " won!", "\nCongratulations!")
-                    U1 += 1
+                    roundCounter += 1
+                    user_one_wins += 1
                 if USER2 in ["scissors", "s"]:
                     print(USER_NAME2, " won!", "\nCongratulations!")
-                    U2 += 1
-            if USER1 in ["scissors", "s"]:
+                    roundCounter += 1
+                    user_two_wins += 1
+            elif USER1 in ["scissors", "s"]:
                 if USER2 in ["rock", "r"]:
                     print(USER_NAME2, " won!", "\nCongratulations!")
-                    U2 += 1
+                    roundCounter += 1
+                    user_two_wins += 1
                 if USER2 in ["paper", "p"]:
                     print(USER_NAME1, " won!", "\nCongratulations!")
-                    U1 += 1
+                    roundCounter += 1
+                    user_one_wins += 1
             elif USER1 == USER2:
                 print("Draw")
             else:
                 print("Error... Try again")
 
-            print(USER_NAME1, ": ", U1, " vs ", USER_NAME2, ": ", U2)
+            print(USER_NAME1, ": ", user_one_wins, " vs ", USER_NAME2, ": ", user_two_wins)
             
         print('G A M E  O V E R')
         
-        if U1 == 10:
-            print(USER_NAME1, ' won the game !')
-        elif U2 == 10:
-            print(USER_NAME2, ' won the game !')
+        if user_one_wins > user_two_wins:
+            print(USER_NAME1, " won the game !")
+        elif user_two_wins > user_one_wins:
+            print(USER_NAME2, " won the game !")
+        elif user_one_wins == user_two_wins:
+            print("It's a tie!")
             
         txt2 = input("Would you like to play again? [y/n]")
         
@@ -57,6 +66,6 @@ def rockPaperScissors(USER_NAME1, USER_NAME2):
             txt = False
             
                      
-t1 = input("What's your name ? ")
-t2 = input("What's your name ? ")
+t1 = input("What's your name? ")
+t2 = input("What's your name? ")
 rockPaperScissors(t1, t2)
